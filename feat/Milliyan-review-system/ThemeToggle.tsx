b9ca03b943c3836bdd089944e694e61097b64357
Milliyan <126200962+Milliyan@users.noreply.github.com>
2026-07-22 11:02:40 -0400

    Update ThemeToggle.tsx

--- feat/Milliyan-review-system/ThemeToggle.tsx
+++ feat/Milliyan-review-system/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
 import { useTheme } from 'next-themes';
 import { Moon, Sun } from 'lucide-react';
 
