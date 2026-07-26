864ca81a334e013f5d527b64aacba9f632776725
abrham111 <abrhambayih733@gmail.com>
2026-07-26 10:58:34 +0300

    add fixes and updates

--- src/common/encryption/encryption.module.ts
+++ src/common/encryption/encryption.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EncryptionService } from './encryption.service';
+import { ENCRYPTION_KEY } from './encryption.constants';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [EncryptionService],
+  providers: [
+    {
+      provide: ENCRYPTION_KEY,
+      useFactory: (config: ConfigService) => {
+        const raw = config.get<string>("ENCRYPTION_KEY");
+
+        if (!raw) {
+          throw new Error("ENCRYPTION_KEY is required");
+        }
+
+        return Buffer.from(raw, "hex");
+      },
+      inject: [ConfigService],
+    },
+    EncryptionService,
+  ],
   exports: [EncryptionService],
 })
 export class EncryptionModule {}
