e693288840e8950156393fde737774c690be009b
Ibsa Abera <ibsaabera712@gmail.com>
2026-07-27 20:03:28 +0300

    fix: make DROP INDEX idempotent in recreate_audit_indexes migration

--- prisma/migrations/20260718142112_recreate_audit_indexes/migration.sql
+++ prisma/migrations/20260718142112_recreate_audit_indexes/migration.sql
@@ -1,5 +1,5 @@
 -- DropIndex
-DROP INDEX "idx_events_log_created_at";
+DROP INDEX IF EXISTS "idx_events_log_created_at";
 
 -- CreateIndex
 CREATE INDEX "idx_events_log_created_at" ON "events_log"("createdAt");
